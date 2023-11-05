034c648697edc558e5c11fcb919bb6e8bece0633
m_santh@yahoo.com <m_santh@yahoo.com>
2023-11-05 10:49:57 +0530

    new changes

--- update_trigger_output.py
+++ update_trigger_output.py
@@ -1,30 +1,31 @@
-import sqlite3
+import time
+import pandas as pd
 # import json
 
-def generate_jsonl_file():
-    c.execute('SELECT * FROM my_table')
-    rows = c.fetchall()
-    column_names = [column[0] for column in c.description]
+def generate_jsonl_file(csvFile):
+    column_names = csvFile.columns
     
     with open('data/data.jsonl', 'w') as f:
-        for row in rows:
-            inner_dict_str = str({column_names[i]: row[i] for i in range(len(column_names))})
+        for row in range(len(csvFile)):
+            print(column_names)
+            print(csvFile.iat[row, 0])
+            print()
+            inner_dict_str = str({column_names[i]: csvFile.iat[row,i] for i in range(len(column_names))})
             outer_json_str = '{"doc": "' + inner_dict_str.replace('"', '\\"') + '"}'
             f.write(outer_json_str + '\n')
 
 # Initialize last_known_row_count
 last_known_row_count = 0
 
-conn = sqlite3.connect('nutri_nfhs5_india.db')
-c = conn.cursor()
 
 while True:  
     # Check current row count
-    c.execute('SELECT COUNT(*) FROM my_table')
-    current_row_count = c.fetchone()[0]
+    csvFile = pd.read_csv('30Days_Commodity_Prices.csv')
+    current_row_count = len(csvFile)
     
     # Compare with last known row count
     if current_row_count != last_known_row_count:
-        generate_jsonl_file()
+        generate_jsonl_file(csvFile)
         last_known_row_count = current_row_count
-
+    else:
+        time.sleep(1)
